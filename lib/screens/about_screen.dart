30a1021231668d7594ae5e4ed3579c5ae1574e7e
AM <ammar260813@gmail.com>
2021-08-02 18:56:45 +0200

    "About Us" screen placeholder text + image Addition of packages in "Prices" screen

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -57,6 +57,38 @@ class AboutUsState extends State<AboutUs> {
               )
           ),
         ),
+
+        Container(
+          padding: EdgeInsets.all(20),
+          child: Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+                  "sed do eiusmod tempor incididunt ut labore et dolore "
+                  "magna aliqua. Ut enim ad minim veniam, quis nostrud "
+                  "exercitation ullamco laboris nisi ut aliquip ex ea "
+                  "commodo consequat. Duis aute irure dolor in reprehenderit "
+                  "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+                  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
+                  " officia deserunt mollit anim id est laborum.",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w200
+              )
+          ),
+        ),
+
+        Container(
+          height: 250,
+          width: 200,
+          padding: EdgeInsets.all(20),
+          child :ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(30),
+            ),
+            child: Image.asset(
+              "assets/images/image00013.jpeg",
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ],
     );
   }
